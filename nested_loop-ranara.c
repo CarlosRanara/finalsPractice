2d401f5656eac6b4f014d74faf536fd5dc375ffe
CarlosRanara <carlosranara0@gmail.com>
2024-11-25 20:43:16 +0800

    rah

---
+++ nested_loop-ranara.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+	Task: Implement the generateTeamNumbers() function
+	
+	This function will generate the team numbers and their respective jersey numbers.
+	Refer to the sample runs for the display format.
+	
+	@param: teams - number of teams to generate jerseys for
+*/
+void generateTeamNumbers(int teams) {
+
+}
+
+int main(){
+
+	int teams;
+	
+	printf("How many teams? ");
+	scanf("%d", &teams);
+	printf("\n");
+	
+	generateTeamNumbers(teams);
+	
+	return 0;
+}
+
